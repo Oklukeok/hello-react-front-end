c820182551ea81f1567033c2f2bb4180fb27de84
Oklukeok <lucasabermudez@gmail.com>
2023-03-17 16:52:12 -0600

    Setup react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './pages/Greetings';
+
+function App() {
+  return (
+    
+    <Greeting />
+    
+  );
+}
+
+export default App;
